047e610e8b55a13a21f21cec86a7c5e5e211f4b5
Gabriel <gopxfs@hotmail.com>
2022-10-20 15:12:44 -0300

    Add devise configuration & migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,46 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_20_175336) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_20_180213) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cars", force: :cascade do |t|
+    t.string "model"
+    t.string "color"
+    t.string "image"
+    t.integer "range"
+    t.string "motor_type"
+    t.decimal "acceleration_time"
+    t.decimal "booking_price"
+    t.integer "booking_duration"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reservations", force: :cascade do |t|
+    t.date "booking_date"
+    t.bigint "user_id", null: false
+    t.bigint "car_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["car_id"], name: "index_reservations_on_car_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", default: "New user"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "reservations", "cars"
+  add_foreign_key "reservations", "users"
 end
