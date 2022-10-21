0a451c5ab713734e46822a2f12e9569a3f3a56c5
Gabriel <gopxfs@hotmail.com>
2022-10-21 17:37:30 -0300

    Add API endpoint to display user's reserved cars Note: Since the models aren't ready yet, the method shouldn't, but the endpoint is working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,4 +12,15 @@ class UsersController < ApplicationController
       format.json { render json: @users }
     end
   end
+
+  def reserved_cars
+    @cars = current_user.reserved_cars
+
+    response.set_header('Access-Control-Allow-Origin', '*')
+
+    respond_to do |format|
+      format.html { render json: @cars }
+      format.json { render json: @cars }
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :users
   resources :cars
 
+  get 'users/:id/reserved_cars', :to => 'users#reserved_cars'
+
   root to: 'users#index'
 end
