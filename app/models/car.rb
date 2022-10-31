95342906eb0fe8e93c363efb96120243e6b60985
waqas ahmad <waqaskanju@gmail.com>
2022-10-31 20:28:43 +0500

    add cars validation

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,2 +1,6 @@
 class Car < ApplicationRecord
+    validates :model , presence: true
+    validates :description , presence: true
+    validates :booking_price , presence: true
+    validates :booking_duration , presence: true
 end
--- db/migrate/20221020175858_create_cars.rb
+++ db/migrate/20221020175858_create_cars.rb
@@ -1,17 +1,23 @@
 class CreateCars < ActiveRecord::Migration[7.0]
   def change
     create_table :cars do |t|
-      t.string :model
+      t.string :model, null: false
       t.string :color
       t.string :image
-      t.string :description
+      t.string :description, null: false
       t.integer :range
       t.string :motor_type
       t.decimal :acceleration_time
       t.decimal :booking_price, precision: 22, scale: 2
-      t.integer :booking_duration
+      t.integer :booking_duration, null: false
 
       t.timestamps
     end
   end
 end
+
+#  rails db:rollback
+#  rails db:migrate
+#  or
+
+#  rails db:migrate:redo
