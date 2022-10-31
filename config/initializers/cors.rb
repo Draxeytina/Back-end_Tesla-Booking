a62327bca2d763e7db63538887014611ebade3ce
waqas ahmad <waqaskanju@gmail.com>
2022-10-31 17:56:25 +0500

    add cors file and password digest

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,18 @@
+
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
-    resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete]
+    origins 'http://localhost:3000'
+    resource '*',
+             headers: :any,
+             methods: %i[get post put patch delete options head],
+             credentials: true
+  end
+
+  allow do
+    origins 'http://abc.com'
+    resource '*',
+             headers: :any,
+             methods: %i[get post put patch delete options head],
+             credentials: true
   end
 end
