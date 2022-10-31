a864b27a54661c225c443b0cf3cd191446972818
Gabriel <gopxfs@hotmail.com>
2022-10-31 15:02:04 -0300

    Update branch

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,4 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[show edit update destroy]
-  before_action :authenticate_user!
 
   def index
     @users = User.all
