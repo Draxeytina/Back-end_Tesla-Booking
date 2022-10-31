407780b46ed50e1f0e331e63a9feb12ce986120e
Gabriel <gopxfs@hotmail.com>
2022-10-31 17:07:34 -0300

    Update localhost port

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
+    origins 'http://localhost:3001'
     resource '*',
              headers: :any,
              methods: %i[get post put patch delete options head],
