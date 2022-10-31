50493fec951bf988dfc0b29d8271fa3c0760faf7
waqas ahmad <waqaskanju@gmail.com>
2022-10-31 20:32:45 +0500

    add user validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,8 @@ class UsersController < ApplicationController
   before_action :set_user, only: %i[show edit update destroy]
   before_action :authenticate_user!
 
+  validates :email , presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
   def index
     @users = User.all
 
