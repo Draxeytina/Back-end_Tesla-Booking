983de53ed18a7a1b6f1023cc8e7a4f5e8ceeb0d5
waqas ahmad <waqaskanju@gmail.com>
2022-10-31 18:05:03 +0500

    add session and registration controller  and current user concern

---
+++ app/controllers/concerns/current_user_concern.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module CurrentUserConcern
+    extend ActiveSupport::Concern
+  
+    included do
+      before_action :set_current_user
+    end
+  
+    def set_current_user
+      @current_user = User.find(session[:user_id]) if session[:user_id]
+    end
+  end
+  
