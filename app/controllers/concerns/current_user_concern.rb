d12b1418eecef5d4680762ecf04483d47eabf62e
waqas ahmad <waqaskanju@gmail.com>
2022-11-02 23:52:53 +0500

    add current user concern

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -5,6 +5,8 @@ module CurrentUserConcern
     before_action :set_current_user
   end
 
+  #  This concern is  used to setup the current user.
+  
   def set_current_user
     @current_user = User.find(session[:user_id]) if session[:user_id]
   end
