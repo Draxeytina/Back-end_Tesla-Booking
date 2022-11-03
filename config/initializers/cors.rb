81fc021294e7558569804f7c7aaee2a8881fe930
Gabriel <gopxfs@hotmail.com>
2022-11-02 22:04:42 -0300

    Reverse route URL changes

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,17 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://tesla-booking.herokuapp.com/'
     resource '*',
              headers: :any,
-             methods: %i[get post put patch delete options head]
+             methods: %i[get post put patch delete options head],
+             credentials: true
+  end
+
+  allow do
+    origins 'https://secret-ridge-50120.herokuapp.com/'
+    resource '*',
+             headers: :any,
+             methods: %i[get post put patch delete options head],
+             credentials: true
   end
 end
